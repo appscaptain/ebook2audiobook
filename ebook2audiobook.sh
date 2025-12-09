f3ef85ffbacfdbd4082ad7d8f63d23c6c441356d
appscaptain <anders+github@appscaptain.com>
2025-12-09 12:17:15 +0100

    Comment out device package installation in Docker mode
    Comment out the installation of device packages in Docker mode.

--- ebook2audiobook.sh
+++ ebook2audiobook.sh
@@ -718,7 +718,7 @@ else
 			echo "Docker image ready! to run your docker: docker run --gpus all -it --rm -p 7860:7860 $DOCKER_IMG_NAME [--options]"
 		elif [[ "$DOCKER_DEVICE_STR" != "" ]];then
 			install_python_packages || exit 1
-			install_device_packages "${DOCKER_DEVICE_STR}" || exit 1
+#			install_device_packages "${DOCKER_DEVICE_STR}" || exit 1
 			check_sitecustomized || exit 1
 		fi
 	elif [[ "$SCRIPT_MODE" == "$NATIVE" ]]; then
